30086e48b9691ffe94f1e48d3964da7e302fd50e
BlueBird <kirk.maningat@gmail.com>
2022-02-13 12:25:24 +0900

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Playground">Playground</div>
+      <div className="Finish">Finish</div>
+      <div className="Doll">Doll</div>
+      <div className="Contestant">Contestant</div>
+    </div>
+  );
+}
+
+export default App;
