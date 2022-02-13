a5c776f960c4a8aab9d277271c1c70cc73111d56
BlueBird <kirk.maningat@gmail.com>
2022-02-13 14:16:57 +0900

    created contestants

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import Contestant from './components/Contestant/Contestant';
 import Doll from './components/Doll/Doll';
 import Finish from './components/Finish/Finish';
 import Playground from './components/Playground/Playground';
 
+type ContestantType = {
+  x: number;
+  y: number;
+  name: string;
+};
 function App() {
+  const [state, setState] = useState({});
+  const contestants = useRef<ContestantType[]>([]);
+
+  useEffect(() => {
+    for (let i = 0; i < 20; i++) {
+      contestants.current.push({
+        x: Math.random() * (1277.33 - 50),
+        y: 1199,
+        name: i.toString(),
+      });
+    }
+
+    setState({});
+  }, []);
   return (
     <div className="App">
       <Playground>
         <Finish />
         <Doll />
-        <Contestant />
+        {contestants.current.map((c) => {
+          return <Contestant x={c.x} y={c.y} name={c.name} />;
+        })}
       </Playground>
     </div>
   );
--- src/components/Contestant/Contestant.tsx
+++ src/components/Contestant/Contestant.tsx
@@ -1,5 +1,15 @@
 import './Contestant.css';
 
-export default function Contestant() {
-  return <div className="Contestant">0</div>;
+type Props = {
+  x: number;
+  y: number;
+  name: string;
+};
+
+export default function Contestant({ x, y, name }: Props) {
+  return (
+    <div style={{ left: `${x}px`, top: `${y}px` }} className="Contestant">
+      {name}
+    </div>
+  );
 }
